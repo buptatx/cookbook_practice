0b9e4084c4d3b473cb340ebc1315cbeb1fc0cd0b
zhangpeng <zhp@chinabr.net>
2019-03-07 13:46:52 +0800

    add func show

--- stack_test.py
+++ stack_test.py
@@ -2,22 +2,25 @@
 
 class Stack(object):
     def __init__(self):
-        self.item = []
+        self.stack = []
 
     def push(self, ele):
-        self.item.append(ele)
+        self.stack.append(ele)
 
     def pop(self):
         if self.is_empty():
             return "stack is empty"
         else:
-            return self.item.pop()
+            return self.stack.pop()
 
     def size(self):
-        return len(self.item)
+        return len(self.stack)
 
     def is_empty(self):
         return self.size() == 0
+
+    def show(self):
+        print(self.stack)
 
 
 if __name__ == "__main__":
@@ -26,6 +29,7 @@ if __name__ == "__main__":
     s.push(2)
     s.push(9)
     s.push(3)
+    s.show()
 
     for i in range(0, 6):
         print("current size: %d , pop element :%s " % (s.size(), s.pop()))
